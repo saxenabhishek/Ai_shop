db41dbbd1acd1432077a7ec70a0a6e63abaa5aea
saxenabhishek <as7122000@gmail.com>
2021-04-30 22:39:12 +0530

    🩹 responsive wrapper

---
+++ code/frontend/src/components/ResponsiveWrapper/ResponsiveWrapper.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./ResponsiveWrapper.module.css";
+
+const ResponsiveWrapper = (props) => {
+  const classNames = [styles.ResponsiveWrapper];
+  if (props.loading) {
+    classNames.push(styles.Loading);
+  }
+  return <div className={classNames.join(" ")}>{props.children}</div>;
+};
+
+export default ResponsiveWrapper;
+
+/**
+ * This function returns the className
+ * so you can use it with any component
+ */
+export const getClassName = () => {
+  return styles.ResponsiveWrapper;
+};
